6ebb1f30b4d2a7158402045ae79da19d2591651a
Yeison Fernandez <jeison_fable@hotmail.com>
2019-08-14 21:42:35 -0600

    Initial Committ
    Initial Committ for Project, Centro Creciendo Juntos

---
+++ CreciendoJuntos/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CreciendoJuntos.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Centro Creciendo Juntos";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contacto";
+
+            return View();
+        }
+
+        public ActionResult Personal()
+        {
+            ViewBag.Message = "Personal";
+
+            return View();
+        }
+
+        public ActionResult Mision()
+        {
+            ViewBag.Message = "Misión, Visión y Valores";
+
+            return View();
+        }
+
+        public ActionResult Servicios()
+        {
+            ViewBag.Message = "Servicios";
+
+            return View();
+        }
+
+        public ActionResult Instalaciones()
+        {
+            ViewBag.Message = "Instalaciones";
+
+            return View();
+        }
+
+        public ActionResult Matricula()
+        {
+            ViewBag.Message = "Matrícula";
+
+            return View();
+        }
+    }
+}
